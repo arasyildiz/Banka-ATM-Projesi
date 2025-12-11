aebfb101b13518ef3f674c84d3fbc8fb3a7aacfe
Aras Yıldız <efearasyildiz@gmail.com>
2025-12-11 23:15:06 +0300

    transer et metodu eklendi

--- main.py
+++ main.py
@@ -27,10 +27,17 @@ class BankaHesabi:
             print(f' -> {islem}')
         print("--------------------\n")
         
-        
+    def transfer_et(self, alici_hesap, miktar):
+        if miktar > self.bakiye:
+            print(f"HATA : {self.isim} hesanbinda yeterli miktarda para bulunmamaktadir")
+        else:
+            self.para_cek(miktar)
+            alici_hesap.para_yatir(miktar)
+            print(f'Sistem : {self.isim} hesabindan {alici_hesap.isim} hesabına {miktar} para gönderildi')
     
 hesap1 = BankaHesabi("Aras", 1000)
-hesap1.para_yatir(500)
-hesap1.para_cek(1300)
-hesap1.para_cek(6000)
+hesap2 = BankaHesabi("Tayfun", 1000)
+hesap1.transfer_et(hesap2, 200)
+
 hesap1.bilgi_ver()
+hesap2.bilgi_ver()
