29948e680e5154f4f4c41ed322129b0984e55ba0
Aras Yıldız <efearasyildiz@gmail.com>
2025-12-11 22:36:20 +0300

    Banka hesabi class yapisi ve bilgi_ver metodu eklendi

---
+++ main.py
@@ -0,0 +1,25 @@
+class BankaHesabi:
+    def __init__(self, sahip_ismi, baslangic_parasi):
+        self.isim = sahip_ismi
+        self.bakiye = baslangic_parasi
+        print(f'Sistem : {self.isim} hesabi basariyla acildi')
+        
+    def para_yatir(self, miktar):
+        self.bakiye += miktar
+        print(f'Sistem : {self.isim} hesabina {miktar} para yatirildi')
+        
+    def para_cek(self, miktar):
+        if miktar > self.bakiye:
+            print(f"HATA : {self.isim} hesanbinda yeterli miktarda para bulunmamaktadir")
+        else :    
+            self.bakiye -= miktar
+            print(f'Sistem : {self.isim} hesabindan {miktar} para cekildi')
+    
+    def bilgi_ver(self):
+            print(f"--- HESAP BİLGİSİ ---\nSayın {self.isim}, bakiyeniz: {self.bakiye} TL\n---------------------")
+    
+    
+hesap1 = BankaHesabi("Aras", 1000)
+hesap1.para_yatir(500)
+hesap1.para_cek(1300)
+hesap1.bilgi_ver()
