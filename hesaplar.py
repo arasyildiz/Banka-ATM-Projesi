d4132d3df8a62142db0bd4358f6ee03577cdfe56
Aras Yıldız <efearasyildiz@gmail.com>
2025-12-12 20:34:54 +0300

    Kodlar modüllere ayrıldı

---
+++ hesaplar.py
@@ -0,0 +1,50 @@
+class BankaHesabi:
+    def __init__(self, sahip_ismi, baslangic_parasi):
+        self.isim = sahip_ismi
+        self.bakiye = baslangic_parasi
+        self.hareketler = []
+        print(f'Sistem : {self.isim} hesabi basariyla acildi')
+        
+    def para_yatir(self, miktar):
+        self.bakiye += miktar
+        self.hareketler.append(f"YATIRILAN : +{miktar} TL")
+        print(f'Sistem : {self.isim} hesabina {miktar} para yatirildi')
+        
+    def para_cek(self, miktar):
+        if miktar > self.bakiye:
+            self.hareketler.append(f"REDDEDİLEN CEKİM : -{miktar} TL (Yetersiz Bakiye)")
+            print(f"HATA : {self.isim} hesanbinda yeterli miktarda para bulunmamaktadir")
+            return False
+        else :    
+            self.bakiye -= miktar
+            self.hareketler.append(f"CEKİLEN : -{miktar} TL")
+            print(f'Sistem : {self.isim} hesabindan {miktar} para cekildi')
+            return True
+        
+    def bilgi_ver(self):
+        print(f"--- HESAP BİLGİSİ ---\nSayın {self.isim}, bakiyeniz: {self.bakiye} TL\n---------------------")
+
+        for islem in self.hareketler:
+            print(f' -> {islem}')
+        print("--------------------\n")
+        
+    def transfer_et(self, alici_hesap, miktar):
+        if self.para_cek(miktar):
+            alici_hesap.para_yatir(miktar)
+            print(f'Sistem : {self.isim} hesabindan {alici_hesap.isim} hesabına {miktar} para gönderildi')
+        else:
+            print(f"HATA : {self.isim} hesanbinda yeterli miktarda para bulunmamaktadir")
+            
+
+class VadeliHesap(BankaHesabi):
+    def __init__(self, sahip_ismi, baslangic_parasi, faiz_orani):
+        super().__init__(sahip_ismi, baslangic_parasi)
+        self.faiz_orani = faiz_orani
+
+
+        
+    def faiz_islet(self):
+        gelen_faiz = (self.bakiye * self.faiz_orani / 100)   
+        self.bakiye += gelen_faiz
+        self.hareketler.append(f"FAİZ GELİRİ: +{gelen_faiz} TL (%{self.faiz_orani})")
+        print(f"Sistem: Faiz işletildi. Kazanılan: {gelen_faiz} TL")
--- main.py
+++ main.py
@@ -1,56 +1,12 @@
-class BankaHesabi:
-    def __init__(self, sahip_ismi, baslangic_parasi):
-        self.isim = sahip_ismi
-        self.bakiye = baslangic_parasi
-        self.hareketler = []
-        print(f'Sistem : {self.isim} hesabi basariyla acildi')
-        
-    def para_yatir(self, miktar):
-        self.bakiye += miktar
-        self.hareketler.append(f"YATIRILAN : +{miktar} TL")
-        print(f'Sistem : {self.isim} hesabina {miktar} para yatirildi')
-        
-    def para_cek(self, miktar):
-        if miktar > self.bakiye:
-            self.hareketler.append(f"REDDEDİLEN CEKİM : -{miktar} TL (Yetersiz Bakiye)")
-            print(f"HATA : {self.isim} hesanbinda yeterli miktarda para bulunmamaktadir")
-            return False
-        else :    
-            self.bakiye -= miktar
-            self.hareketler.append(f"CEKİLEN : -{miktar} TL")
-            print(f'Sistem : {self.isim} hesabindan {miktar} para cekildi')
-            return True
-        
-    def bilgi_ver(self):
-        print(f"--- HESAP BİLGİSİ ---\nSayın {self.isim}, bakiyeniz: {self.bakiye} TL\n---------------------")
+from hesaplar import BankaHesabi, VadeliHesap
 
-        for islem in self.hareketler:
-            print(f' -> {islem}')
-        print("--------------------\n")
-        
-    def transfer_et(self, alici_hesap, miktar):
-        if self.para_cek(miktar):
-            alici_hesap.para_yatir(miktar)
-            print(f'Sistem : {self.isim} hesabindan {alici_hesap.isim} hesabına {miktar} para gönderildi')
-        else:
-            print(f"HATA : {self.isim} hesanbinda yeterli miktarda para bulunmamaktadir")
-            
+print("--- BANKA SİSTEMİ BAŞLATILIYOR ---\n")
 
-class VadeliHesap(BankaHesabi):
-    def __init__(self, sahip_ismi, baslangic_parasi, faiz_orani):
-        super().__init__(sahip_ismi, baslangic_parasi)
-        self.faiz_orani = faiz_orani
+aras_hesap = BankaHesabi("Aras Normal", 500)
+aras_hesap.para_yatir(100)
 
+vadeli_hesap = VadeliHesap("Aras Vadeli", 2000, 15)
+vadeli_hesap.faiz_islet()
 
-        
-    def faiz_islet(self):
-        gelen_faiz = (self.bakiye * self.faiz_orani / 100)   
-        self.bakiye += gelen_faiz
-        self.hareketler.append(f"FAİZ GELİRİ: +{gelen_faiz} TL (%{self.faiz_orani})")
-        print(f"Sistem: Faiz işletildi. Kazanılan: {gelen_faiz} TL")
-        
-        
-vadeli1 = VadeliHesap("Aras Vadeli", 1000, 10)
-vadeli1.para_yatir(500) 
-vadeli1.faiz_islet()
-vadeli1.bilgi_ver()
+aras_hesap.bilgi_ver()
+vadeli_hesap.bilgi_ver()
